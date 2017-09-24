8f75a553e04b82ac6623b2044c7db01efd030334
wwymak <wwymak@gmail.com>
2017-09-24 20:05:45 +0100

    project

---
+++ index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const express = require("express");
+const http = require('http');
+const geocoder = require('geocoder');
+// Setup server
+const app = express();
+const bodyParser = require('body-parser');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+const Twitter = require('node-tweet-stream');// require('twitter');
+
+const credentials = require('./config/twitterConf');
+
+const client = new Twitter({
+    consumer_key: process.env.twitConsumerKey || credentials.consumer.key,
+    consumer_secret: process.env.twitConsumerSecret || credentials.consumer.secret,
+    token: process.env.twitAcessToken ||credentials.access_token.key,
+    token_secret: process.env.twitAcessTokenSecret || credentials.access_token.secret
+});
+
+
+client.on('tweet', function(tweet) {
+
+    let obj = {
+        id: tweet.id,
+        coordinates: tweet.coordinates,
+        created_at: tweet.created_at,
+        text: tweet.text,
+        userLoc: tweet.user.location,
+        userName: tweet.user.name
+    };
+    if(tweet.coordinates && tweet.coordinates.coordinates ) {
+        tweet.coordinates= tweet.coordinates.coordinates;
+        console.log(obj)
+        io.emit('tweet', obj);
+    } else if(tweet.user.location) {
+        geocoder.geocode(tweet.user.location, (err, data) => {
+            if(!err && data.results && data.results.length > 0) {
+                console.log(data.results[0].geometry)
+                let latlng = data.results[0].geometry.location;
+                obj.coordinates=[latlng.lng, latlng.lat]
+                console.log(obj);
+                io.emit('tweet', obj);
+            } else {
+                io.emit('tweet', obj);
+            }
+        })
+    } else {
+        io.emit('tweet', obj);
+    }
+
+
+});
+
+client.on('error', function (err) {
+    console.log('error!!!', err);
+});
+
+client.track('earthquake,mexicoquake');
+
+io.on('connection', function (socket) {
+    console.log('connection')
+    socket.emit('news', { hello: 'world' });
+});
+
+app.set('port', (process.env.PORT || 3000));
+
+server.listen(app.get('port'), () => {
+    console.log('Express server listening on %d', app.get('port'));
+});
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Api Routes
+// app.use('/api/get-towers', require('./api/getTowersInfo'));
+// app.use('/api/get-carriers-count', require('./api/getCarriersInfo'));
+app.use(express.static('public'));
+
+// All other routes should redirect to the index.html
+app.route('/')
+    .get((req, res) => {
+        res.sendFile(__base + 'public/index.html')
+    });
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
---
+++ public/main.js
@@ -0,0 +1,127 @@
+(function () {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoid3d5bWFrIiwiYSI6IkxEbENMZzgifQ.pxk3bdzd7n8h4pKzc9zozw';
+    const socket = io('http://localhost:3000');
+    let tweetTextArr = [];
+    let latLngArr = [];
+    let convertLngLatToGeojson = function (latLngArr) {
+        let geojson = {
+            "type": "FeatureCollection",
+            "features": []
+        };
+        latLngArr.forEach(d => geojson.features.push({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [d[0], d[1]]
+            }
+        }));
+        return geojson;
+    };
+    let plotMarkers = function (map, geojsonData) {
+        if(map.getSource('geojsonData')){
+            map.getSource('geojsonData').setData(geojsonData);
+            return;
+        }
+        map.addSource("geojsonData", {
+            type: "geojson",
+            data: geojsonData,
+            cluster: true,
+            clusterMaxZoom: 15, // Max zoom to cluster points on
+            clusterRadius: 20
+        });
+
+        let layers = [
+            [0, 'green'],
+            [10, 'orange'],
+            [100, 'red']
+        ];
+
+        layers.forEach(function (layer, i) {
+            map.addLayer({
+                "id": "cluster-" + i,
+                "type": "circle",
+                "source": "geojsonData",
+                "paint": {
+                    "circle-color": layer[1],
+                    "circle-radius": 70,
+                    "circle-blur": 1 // blur the circles to get a heatmap look
+                },
+                "filter": i === layers.length - 1 ?
+                    [">=", "point_count", layer[0]] :
+                    ["all",
+                        [">=", "point_count", layer[0]],
+                        ["<", "point_count", layers[i + 1][0]]]
+            });
+        });
+
+        map.addLayer({
+            id: 'tweetLocations',
+            type: 'circle',
+            source: 'geojsonData',
+            "paint": {
+                "circle-color": 'rgba(0,255,0,0.5)',
+                "circle-radius": 20,
+                "circle-blur": 1
+            },
+            "filter": ["!=", "cluster", true]
+        });
+
+    };
+
+    let pushQueue = function (data) {
+        let maxLen = 10;
+        if(tweetTextArr.length < maxLen) {
+            tweetTextArr.push(data);
+        } else {
+            let tweetTextArr2 = tweetTextArr.splice(1);
+            tweetTextArr2.push(data);
+            tweetTextArr = tweetTextArr2;
+        }
+    };
+    let tweetDisplay = function (tweetTextArr) {
+        let htmlString = "";
+        tweetTextArr.forEach(d => {
+            htmlString += `
+            <div>
+                <h4>${d.userName}</h4>
+                <p>${d.text}</p>
+            </div>
+            `
+        })
+        $("#tweets").empty();
+        $("#tweets").append(htmlString);
+    }
+    // map setup
+    let mapDisplay = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/wwymak/cj5mix0vr3gom2rqovhi5cix9',
+        zoom: 2,
+        center: [-95, 35]
+    });
+    mapDisplay.addControl(new mapboxgl.NavigationControl());
+
+
+
+    socket.on('tweet', function (data) {
+        // console.log(data);
+        if(data.coordinates) {latLngArr.push(data.coordinates)};
+        pushQueue(data);
+        tweetDisplay(tweetTextArr);
+    });
+
+    let ticks = 0;
+    let timer = setInterval(() => {
+        // latLngArr = latLngArr.slice(0.5 * latLngArr.length, latLngArr.length)
+        let geojson = convertLngLatToGeojson(latLngArr);
+        plotMarkers(mapDisplay, geojson);
+        ticks+=1;
+        if(ticks > 20) {
+            latLngArr = latLngArr.slice(0.5 * latLngArr.length, latLngArr.length);
+            ticks=0;
+        }
+    }, 3000);
+
+    let stopTime = function() {
+        clearInterval(timer);
+    }
+})();
